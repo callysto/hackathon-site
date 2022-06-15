d7e9c8bab70a47cb5f12bcaaeec74a5a7c01d2ed
jlee2843 <96640495+jlee2843@users.noreply.github.com>
2022-06-14 19:13:27 -0600

    fixed errors

--- exercises/exc_02_02.py
+++ exercises/exc_02_02.py
@@ -2,7 +2,11 @@ import pandas as pd #importing the pandas library
 url = 'https://raw.githubusercontent.com/callysto/data-files/main/Science/Climatograph/world-bank-climate-data-canada.csv'
 weather = pd.read_csv(url) #the data from the url is read into the weather variable
 
+weather["Location"].unique() #List of all territories that we can look into
+
 temperature = weather[weather["Type"] == "temperature"] #filter data for temperatures and assign to temperature variable
 
-print("June",temperature["Jun"].mean()) #calculate and print the average temperature value for June
-print("August",temperature["Aug"].mean()) #calculate and print the average temperature value for August
+alberta = temperature.loc[temperature["Location"] == "Alberta"] #filter alberta as the region of focus
+
+print("June",alberta["Jun"].median()) #calculate and print the average temperature value for June
+print("August",alberta["Aug"].median()) #calculate and print the average temperature value for August
--- exercises/exc_02_03.py
+++ exercises/exc_02_03.py
@@ -2,7 +2,11 @@ import pandas as pd #importing the pandas library
 url = 'https://raw.githubusercontent.com/callysto/data-files/main/Science/Climatograph/world-bank-climate-data-canada.csv'
 weather = pd.read_csv(url) #the data from the url is read into the weather variable
 
+weather["Location"].unique() #List of all territories that we can look into
+
 precipitation = weather[weather["Type"] == "precipitation"] #filter data for precipitation and assign to precipitation variable
 
-print("June",precipitation["Jun"].mean()) #calculate and print the average precipitation value for June
-print("August",precipitation["Aug"].mean()) #calculate and print the average precipitation value for August
+alberta = precipitation.loc[precipitation["Location"] == "Alberta"] #filter alberta as the region of focus
+
+print("June",alberta["Jun"].mean()) #calculate and print the average precipitation value for June
+print("August",alberta["Aug"].mean()) #calculate and print the average precipitation value for August
